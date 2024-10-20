539fc358b541c03c7c7af2b811402a43b1249d0e
Ujjwal <45ujjwal@gmail.com>
2024-10-20 19:34:59 +0530

    Add New To-Do

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -10,7 +10,7 @@ function TodoForm() {
     const add = (e) => {
         e.preventDefault()
         if(!todo) return 
-        addTodo({todo: todo, completed: "false"})
+        addTodo({todo: todo, completed: false})
         setTodo("")
     } 
 
